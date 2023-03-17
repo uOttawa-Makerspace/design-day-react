44673cfa4cc2c7c77b9dfbe85a85116b7fe00158
NomadNaomie <nao@naomie.digital>
2023-03-17 14:00:59 -0400

    Making 2023 changes

--- src/config/config.ts
+++ src/config/config.ts
@@ -20,34 +20,34 @@ interface Config {
 }
 
 const config: Config = {
-  year: 2022,
-  semester: Semesters.fall,
-  dateFr: "01/12/2022",
-  dateEn: "2022/12/01",
-  sheetId: "1cH8HX01hMJarwz5Idsd6hqybOqJruonuSqn8GgFlXOw",
+  year: 2023,
+  semester: Semesters.winter,
+  dateFr: "30/03/2023",
+  dateEn: "2023/03/30",
+  sheetId: "1edPKFVkc3KErPQHaG4zVG-L95Kz2psM96qxbeWKW7w4",
   generalSchedule: [
     {
-      time: "10h30-11h00",
+      time: "8h30-9h00",
       eventEn: "Judge orientation",
       eventFr: "Orientation des juges",
     },
     {
-      time: "11h15-13h15",
+      time: "9h15-11h15",
       eventEn: "Judging",
       eventFr: "Évaluation",
     },
     {
-      time: "13h15-14h45",
+      time: "11h15-12h45",
       eventEn: "Lunch and Deliberation",
       eventFr: "Diner et déliberation",
     },
     {
-      time: "13h15-15h15",
+      time: "11h15-13h15",
       eventEn: "Event open to the public",
       eventFr: "Evenement ouvert au public",
     },
     {
-      time: "15h15-16h15",
+      time: "13h15-14h15",
       eventEn: "Closing remark and awards ceremony",
       eventFr: "Remarques de clôture et cérémonie de remise des prix",
     },
